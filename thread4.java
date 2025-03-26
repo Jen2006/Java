62d2d11bea82e4e8cd00fa3530a861509d9a6e2e
Jen2006 <jenishacastelino8@gmail.com>
2025-03-26 15:09:53 +0530

    Add files via upload

---
+++ thread4.java
@@ -0,0 +1,51 @@
+package koi;
+
+class BankAccount{
+	private int balance=1000;
+	
+	public synchronized void withdraw(int amount,String name) {
+		if(balance>=amount) {
+			System.out.println(name+"is withdrawing"+amount);
+			
+			try {
+				Thread.sleep(100);
+			}catch(InterruptedException e) {
+				System.out.println(e);
+			}
+			balance-=amount;
+			System.out.println(name+"completed withdrawal. Remaining balance"+balance);
+		}else{
+			System.out.println(name+"attempted to withdrawl.Remaining balance:"+balance);
+		}
+	}
+}
+class Customer extends Thread{
+	private BankAccount account;
+	private int amount;
+	public Customer(BankAccount account,int amount,String name) {
+		super(name);
+		this.amount=amount;
+		this.account=account;
+		
+	}
+	public void run()
+	{
+		account.withdraw(amount,Thread.currentThread().getName());
+		
+	}
+	
+}
+public class thread4{
+	public static void main(String[]args) {
+		BankAccount account=new BankAccount();
+		
+		Customer c1=new Customer(account,700,"Alice");
+		Customer c2=new Customer(account,500,"Bob");
+		Customer c3=new Customer(account,400,"Charlie");
+		
+		c1.start();
+		c2.start();
+		c3.start();
+		
+	}
+}
